33b368e4f958e26d5fa201987c72b1e510a4422f
AdelineRCWG <adelinewyse@gmail.com>
2022-03-16 15:10:22 -0300

    Initial commit

---
+++ view/shared/header.php
@@ -0,0 +1,38 @@
+<?php ?>
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>AwyguiDev</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=Quantico:ital,wght@0,400;0,700;1,700&family=Roboto+Mono:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/./view/css/reset-normalize.css">
+	<link rel="stylesheet" href="/./view/css/style.css">
+	<link rel="stylesheet" href="/./view/css/header.css">
+</head>
+
+<body>
+<header>
+    <div class="head">
+	    <h1 class="title-Site"><a><span class="title-Site1">Awygui</span><span class="title-Site2">Dev</span></a></h1>
+        <nav class="menu-links">
+            <ul class="menu-links-list">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Posts</a></li>
+	            <li><a href="index.php">About</a></li>
+                <li><a href="index.php">Portifolio</a></li>
+	            <li><a href="index.php">Contact</a></li>
+            </ul>
+        </nav>
+	    <nav class="menu-socials">
+	        <ul class="menu-socials-list">
+		        <li><a href="index.php">YT</a></li>
+		        <li><a href="index.php">GitHub</a></li>
+		        <li><a href="index.php">IG</a></li>
+	        </ul>
+        </nav>
+    </div>
+</header>
